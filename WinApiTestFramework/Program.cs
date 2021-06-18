0e2bfc40f75faf2b58c2d7ead30e3d77ba9f169d
Dmytrych <55029343+Dmytrych@users.noreply.github.com>
2021-06-18 09:16:48 +0300

    Add files via upload

---
+++ WinApiTestFramework/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace WinApiTestFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = GetCurrentProcess();
+            string input = Console.ReadLine();
+
+            if (input == "stop")
+            {
+                TerminateProcess(process, 101);
+            }
+            else
+            {
+                Point point = new Point();
+                while (true)
+                {
+                    GetCursorPos(ref point);
+                    Console.WriteLine(point.X);
+                    Thread.Sleep(4);
+                    Console.Clear();
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern GCHandle GetCurrentProcess();
+
+        [DllImport("kernel32.dll")]
+        private static extern void TerminateProcess(GCHandle handler, uint number);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(ref Point point);
+    }
+}
